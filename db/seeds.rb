ba23135e1266f1a35a70083697431f483be55686
joao-meyer <joaoparizotimeyer@gmail.com>
2024-11-29 17:53:56 -0300

    <ruby-on-rails/seed> creates first user seeder.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # Create initial user
 User.create(name: "admin", email: "first.user@email.com", enabled: true, password: "1234561")
+
+p "Created #{User.count} Users"
+
+p "Done seeding first user."
